fb26176a13c5df6944db2cb04b450202c4fb72cc
Michael DiNunzio <mdinunzio@gmail.com>
2020-02-20 21:51:21 -0500

    dl mechanism working

--- main/main.py
+++ main/main.py
@@ -1,1 +1,20 @@
 import scrapekit
+import time
+
+
+def refresh_accounts_and_kill():
+    """
+    Refresh Mint accounts then kill the driver.
+    """
+    ms = scrapekit.MintScraper()
+    ms.refresh_accounts()
+    time.sleep(15 * 60)
+    ms.driver.quit()
+
+
+def download_and_kill():
+    """
+    Download Mint transactions then kill the driver.
+    """
+    ms = scrapekit.MintScraper()
+    ms.download_transactions()
--- main/scrapekit.py
+++ main/scrapekit.py
@@ -1,41 +1,50 @@
 import authapi
 import time
 import os
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+# URLS
 MINT_URL = r'https://www.mint.com'
 
-# File locations
-# this_loc = __file__
-# parent_dir = os.path.abspath(os.path.join(this_loc, '..'))
-# local_dir = os.path.abspath(os.path.join(parent_dir, '..', 'local'))
-# local_dir = r'C:\Users\mdinu\eclipse-workspace\Mint\local'
+# Paths
+DRIVER_LOC = r'C:\Program Files\chromedriver_win32\chromedriver.exe'
+USER_DIR = os.path.expanduser('~')
+DL_DIR = os.path.join(USER_DIR, 'Downloads')
+OPT_DIR = os.path.join(
+    USER_DIR, r'AppData\Local\Google\Chrome\User Data\Default')
 
+# Regular Expressions
+trans_pattern = r'transactions(?: \((?P<instance>\d+)\))?\.csv'
+trans_re = re.compile(trans_pattern)
+
+
+# MODELS #####################################################################
 
 class MintScraper():
     def __init__(self):
-        self.driver_loc = \
-            r'C:\Program Files\chromedriver_win32\chromedriver.exe'
-        self.user_dir = os.path.expanduser('~')
-        self.username = self.user_dir.split('\\')[-1]
+        self.username = USER_DIR.split('\\')[-1]
         self.opt_dir = os.path.join(
-            self.user_dir,
+            USER_DIR,
             r'AppData\Local\Google\Chrome\User Data\Default')
         options = webdriver.ChromeOptions()
 #         options.add_argument(f"user-data-dir={self.opt_dir}")
-        self.driver = webdriver.Chrome(self.driver_loc, options=options)
+        self.driver = webdriver.Chrome(DRIVER_LOC, options=options)
         self.logged_in = False
 
-    def await_element(self, criteria, by=By.CSS_SELECTOR, timeout=10):
+    def await_element(self, criteria,
+                      by_type=By.CSS_SELECTOR,
+                      ec_type=EC.element_to_be_clickable,
+                      timeout=20):
         """
         Return an element on a page after it has finished rendering.
         """
         wdw = WebDriverWait(self.driver, timeout)
-        ec = EC.presence_of_element_located((by, criteria))
+        ec = ec_type((by_type, criteria))
         element = wdw.until(ec)
         return element
 
@@ -56,7 +65,7 @@ class MintScraper():
 
     def refresh_accounts(self, login=None):
         if login is None:
-            login = self.logged_in
+            login = not self.logged_in
         if login:
             self.login()
         gear_btn = self.await_element('#yui_3_2_0pr1_1_158224734217425450')
@@ -66,10 +75,43 @@ class MintScraper():
 
     def download_transactions(self, login=None):
         if login is None:
-            login = self.logged_in
+            login = not self.logged_in
         if login:
             self.login()
         trans_link = self.await_element('li#transaction > a')
         trans_link.click()
         trans_exp = self.await_element('#transactionExport')
+        time.sleep(10)
+        bar_x = self.driver.find_element(By.CSS_SELECTOR,
+                                         '.x.icon.icon-x-white')
+        if bar_x:
+            try:
+                bar_x.click()
+            except Exception as e:
+                print(e)
         trans_exp.click()
+
+
+# FUNCTIONS ##################################################################
+
+def _sort_files(x):
+    """
+    Return the sort key index for a transaction file.
+    """
+    mtch = trans_re.match(x)
+    if mtch['instance']:
+        return int(mtch['instance'])
+    else:
+        return 0
+
+
+def get_latest_file_location():
+    """
+    Return a string representing the location of the most recently
+    downloaded transactions file.
+    """
+    dl_files = os.listdir(DL_DIR)
+    trans_files = [x for x in dl_files if trans_re.match(x)]
+    trans_files.sort(key=_sort_files, reverse=True)
+    trans_file = trans_files[0]
+    return os.path.join(DL_DIR, trans_file)
