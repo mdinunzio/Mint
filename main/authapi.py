165fc880f5dbc1d0d3346187ceec01417f32914d
Michael DiNunzio <mdinunzio@gmail.com>
2020-09-23 21:52:41 -0400

    added gmail functionality

--- main/authapi.py
+++ main/authapi.py
@@ -1,9 +1,16 @@
 import config as cfg
 import json
 import os
+import pickle
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 
 class AuthManager():
+    """
+    A generic Authentication manager.
+    """
+
     def __init__(self, json_file=None):
         self.username = None
         self.password = None
@@ -32,7 +39,62 @@ class AuthManager():
         return self.__str__()
 
 
+class GmailOuath2Manager():
+    """
+    A Google-specific Oauth2 manager.
+    """
+
+    def __init__(self):
+        self.cred_path = os.path.join(cfg.LOCAL_DIR, 'gmail.pickle')
+        self.set_creds()
+
+    def set_creds(self):
+        """
+        Set the credentials from a credentials pickle file.
+        """
+        if not os.path.exists(self.cred_path):
+            self.setup_creds_file()
+
+        with open(self.cred_path, 'rb') as token:
+            creds = pickle.load(token)
+
+        if not creds.valid:
+            if creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                self.setup_creds_file()
+                with open(self.cred_path, 'rb') as token:
+                    creds = pickle.load(token)
+
+        self.creds = creds
+
+    def setup_cred_file(self):
+        """
+        Generate the pickle file needed for Gmail API communication.
+        """
+
+        # Specify permissions to send and read/write messages
+        # Find more information at:
+        # https://developers.google.com/gmail/api/auth/scopes
+        SCOPES = ['https://www.googleapis.com/auth/gmail.send',
+                  'https://www.googleapis.com/auth/gmail.modify']
+
+        json_path = os.path.join(cfg.LOCAL_DIR, 'gmail.json')
+
+        # Indicate to the API how we will be generating our credentials
+        flow = InstalledAppFlow.from_client_secrets_file(json_path, SCOPES)
+
+        # Get the credentials from Google
+        creds = flow.run_local_server(port=0)
+
+        # We are going to store the credentials in the user's home directory
+        self.cred_path = os.path.join(cfg.LOCAL_DIR, 'gmail.pickle')
+        with open(self.cred_path, 'wb') as token:
+            pickle.dump(creds, token)
+
+
 mint = AuthManager('mint.json')
 twilio = AuthManager('twilio.json')
 imgur = AuthManager('imgur.json')
 user_data = AuthManager('user_data.json')
+gmail = GmailOuath2Manager()
---
+++ main/gmail.py
@@ -0,0 +1,83 @@
+import authapi
+import base64
+import googleapiclient.discovery
+from email import encoders
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+
+
+# https://code.activestate.com/recipes/473810/
+
+class EmailMessage():
+    """
+    An interface for sending emails.
+    """
+    def __init__(self, to, message, subject='', from_=None, mode='html',
+                 images=[], attachments=[]):
+        self.service = googleapiclient.discovery.build(
+            'gmail', 'v1', credentials=authapi.gmail.creds)
+        self.to = to
+        self.message = message
+        self.subject = subject
+        if from_ is None:
+            from_ = authapi.user_data.email
+        self.mode = mode
+        self.images = images
+        self.attachments = attachments
+
+    def send(self):
+        """
+        Construct and send a MIME message.
+        """
+        msgRoot = MIMEMultipart('related')
+        msgRoot['Subject'] = self.subject
+        msgRoot['From'] = self.from_
+        msgRoot['To'] = self.to
+        msgRoot.preamble = 'This is a multi-part message in MIME format.'
+
+        # Encapsulate the plain and HTML versions of the
+        # message body in an 'alternative' part, so message
+        # agents can decide which they want to display.
+        msgAlternative = MIMEMultipart('alternative')
+        msgRoot.attach(msgAlternative)
+
+        msgText = MIMEText(self.message, self.mode)
+        msgAlternative.attach(msgText)
+
+        for i, filename in enumerate(self.images):
+            fp = open(filename, 'rb')
+            msg_image = MIMEImage(fp.read())
+            fp.close()
+
+            # Define the image's ID as referenced above
+            msg_image.add_header('Content-ID', f'<image{i+1}>')
+            msgRoot.attach(msg_image)
+
+        for i, filename in enumerate(self.attachments):
+            # Open file in binary mode
+            with open(filename, "rb") as attachment:
+                # Add file as application/octet-stream
+                # Email client can usually download this
+                # automatically as attachment
+                msg_base = MIMEBase("application", "octet-stream")
+                msg_base.set_payload(attachment.read())
+            # Encode file in ASCII characters to send by email
+            encoders.encode_base64(msg_base)
+            # Add header as key/value pair to attachment part
+            msg_base.add_header(
+                "Content-Disposition",
+                f"attachment; filename= {filename}",
+            )
+            msg_base.add_header('X-Attachment-Id', f'{i+1}')
+            msg_base.add_header('Content-ID', f'<Attachment{i+1}>')
+            msgRoot.attach(msg_base)
+
+        raw = base64.urlsafe_b64encode(msgRoot.as_bytes())
+        raw = raw.decode()
+        body = {'raw': raw}
+
+        message = (self.service.users().messages().send(
+            userId="me", body=body).execute())
+        return message['id']
