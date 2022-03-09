d26442b0417b58a846e17470f02be391670cd42d
Michael DiNunzio <mdinunzio@gmail.com>
2022-03-08 21:19:14 -0500

    Add group and subgroup columns to MintTransactions

--- sql/MintTransactions.sql
+++ sql/MintTransactions.sql
@@ -1,4 +1,4 @@
-CREATE TABLE finances."MintTransactions" (
+CREATE TABLE finances."MintTransactions_dev" (
 	"Date" date NULL,
 	"Description" varchar NULL,
 	"OriginalDescription" varchar NULL,
@@ -8,18 +8,22 @@ CREATE TABLE finances."MintTransactions" (
 	"AccountName" varchar NULL,
 	"Labels" varchar NULL,
 	"Notes" varchar NULL,
-	"UpdateDatetime" timestamp NULL
+	"UpdateDatetime" timestamp NULL,
+	"Group" varchar NULL,
+	"Subgroup" varchar NULL
 );
 
 -- Column comments
 
-COMMENT ON COLUMN finances."MintTransactions"."Date" IS 'The date of the transaction.';
-COMMENT ON COLUMN finances."MintTransactions"."Description" IS 'Mint''s description of the transaction.';
-COMMENT ON COLUMN finances."MintTransactions"."OriginalDescription" IS 'The original source''s description of the transaction.';
-COMMENT ON COLUMN finances."MintTransactions"."Amount" IS 'The amount of the transaction.';
-COMMENT ON COLUMN finances."MintTransactions"."TransactionType" IS 'Whether the transaction was a debit or credit.';
-COMMENT ON COLUMN finances."MintTransactions"."Category" IS 'Mint''s categorization of the transaction.';
-COMMENT ON COLUMN finances."MintTransactions"."AccountName" IS 'The source account of the transaction.';
-COMMENT ON COLUMN finances."MintTransactions"."Labels" IS 'Labels from Mint.';
-COMMENT ON COLUMN finances."MintTransactions"."Notes" IS 'Notes from Mint.';
-COMMENT ON COLUMN finances."MintTransactions"."UpdateDatetime" IS 'The timestamp at which the row was last updated.';
+COMMENT ON COLUMN finances."MintTransactions_dev"."Date" IS 'The date of the transaction.';
+COMMENT ON COLUMN finances."MintTransactions_dev"."Description" IS 'Mint''s description of the transaction.';
+COMMENT ON COLUMN finances."MintTransactions_dev"."OriginalDescription" IS 'The original source''s description of the transaction.';
+COMMENT ON COLUMN finances."MintTransactions_dev"."Amount" IS 'The amount of the transaction.';
+COMMENT ON COLUMN finances."MintTransactions_dev"."TransactionType" IS 'Whether the transaction was a debit or credit.';
+COMMENT ON COLUMN finances."MintTransactions_dev"."Category" IS 'Mint''s categorization of the transaction.';
+COMMENT ON COLUMN finances."MintTransactions_dev"."AccountName" IS 'The source account of the transaction.';
+COMMENT ON COLUMN finances."MintTransactions_dev"."Labels" IS 'Labels from Mint.';
+COMMENT ON COLUMN finances."MintTransactions_dev"."Notes" IS 'Notes from Mint.';
+COMMENT ON COLUMN finances."MintTransactions_dev"."UpdateDatetime" IS 'The timestamp at which the row was last updated.';
+COMMENT ON COLUMN finances."MintTransactions_dev"."Group" IS 'The user-defined group for the transaction.';
+COMMENT ON COLUMN finances."MintTransactions_dev"."Subgroup" IS 'The user-defined subgroup for the transaction.';
