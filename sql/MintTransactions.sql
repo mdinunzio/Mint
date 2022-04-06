4dd1bc27f99c5a0ab2da8c4818997d609dc9550b
Michael DiNunzio <mdinunzio@gmail.com>
2022-04-06 09:12:36 -0400

    Fix MinTransactions.sql capitalization and index names

--- sql/MintTransactions.sql
+++ sql/MintTransactions.sql
@@ -1,4 +1,4 @@
-CREATE TABLE finances."MintTransactions_dev" (
+CREATE TABLE finances."MintTransactions" (
 	"Date" date NOT NULL,
 	"Description" varchar NOT NULL,
 	"OriginalDescription" varchar NULL,
@@ -13,37 +13,37 @@ CREATE TABLE finances."MintTransactions_dev" (
 	"Subgroup" varchar NULL
 );
 
-CREATE INDEX "MintTransactions_dev_idx_date" ON finances."MintTransactions_dev" (
+CREATE INDEX "MintTransactions_idx_date" ON finances."MintTransactions" (
     "Date" DESC
 );
 
-CREATE INDEX "MintTransactions_dev_idx_des" ON finances."MintTransactions_dev" (
+CREATE INDEX "MintTransactions_idx_description" ON finances."MintTransactions" (
     "Description"
 );
 
-CREATE INDEX "MintTransactions_dev_idx_type" ON finances."MintTransactions_dev" (
+CREATE INDEX "MintTransactions_idx_transaction_type" ON finances."MintTransactions" (
     "TransactionType"
 );
 
-CREATE INDEX "MintTransactions_dev_idx_grp" ON finances."MintTransactions_dev" (
+CREATE INDEX "MintTransactions_idx_group" ON finances."MintTransactions" (
     "Group"
 );
 
-CREATE INDEX "MintTransactions_dev_idx_sgrp" ON finances."MintTransactions_dev" (
+CREATE INDEX "MintTransactions_idx_subgroup" ON finances."MintTransactions" (
     "Subgroup"
 );
 
 
 
-COMMENT ON COLUMN finances."MintTransactions_dev"."Date" IS 'The date of the transaction.';
-COMMENT ON COLUMN finances."MintTransactions_dev"."Description" IS 'Mint''s description of the transaction.';
-COMMENT ON COLUMN finances."MintTransactions_dev"."OriginalDescription" IS 'The original source''s description of the transaction.';
-COMMENT ON COLUMN finances."MintTransactions_dev"."Amount" IS 'The amount of the transaction.';
-COMMENT ON COLUMN finances."MintTransactions_dev"."TransactionType" IS 'Whether the transaction was a debit or credit.';
-COMMENT ON COLUMN finances."MintTransactions_dev"."Category" IS 'Mint''s categorization of the transaction.';
-COMMENT ON COLUMN finances."MintTransactions_dev"."AccountName" IS 'The source account of the transaction.';
-COMMENT ON COLUMN finances."MintTransactions_dev"."Labels" IS 'Labels from Mint.';
-COMMENT ON COLUMN finances."MintTransactions_dev"."Notes" IS 'Notes from Mint.';
-COMMENT ON COLUMN finances."MintTransactions_dev"."UpdateDatetime" IS 'The timestamp at which the row was last updated.';
-COMMENT ON COLUMN finances."MintTransactions_dev"."Group" IS 'The user-defined group for the transaction.';
-COMMENT ON COLUMN finances."MintTransactions_dev"."Subgroup" IS 'The user-defined subgroup for the transaction.';
+COMMENT ON COLUMN finances."MintTransactions"."Date" IS 'The date of the transaction.';
+COMMENT ON COLUMN finances."MintTransactions"."Description" IS 'Mint''s description of the transaction.';
+COMMENT ON COLUMN finances."MintTransactions"."OriginalDescription" IS 'The original source''s description of the transaction.';
+COMMENT ON COLUMN finances."MintTransactions"."Amount" IS 'The amount of the transaction.';
+COMMENT ON COLUMN finances."MintTransactions"."TransactionType" IS 'Whether the transaction was a debit or credit.';
+COMMENT ON COLUMN finances."MintTransactions"."Category" IS 'Mint''s categorization of the transaction.';
+COMMENT ON COLUMN finances."MintTransactions"."AccountName" IS 'The source account of the transaction.';
+COMMENT ON COLUMN finances."MintTransactions"."Labels" IS 'Labels from Mint.';
+COMMENT ON COLUMN finances."MintTransactions"."Notes" IS 'Notes from Mint.';
+COMMENT ON COLUMN finances."MintTransactions"."UpdateDatetime" IS 'The timestamp at which the row was last updated.';
+COMMENT ON COLUMN finances."MintTransactions"."Group" IS 'The user-defined group for the transaction.';
+COMMENT ON COLUMN finances."MintTransactions"."Subgroup" IS 'The user-defined subgroup for the transaction.';
